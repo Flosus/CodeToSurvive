61b0dd07f6c6a59b2fee5358839b8431666dedc5
Flosus <flosus.mail@gmail.com>
2023-07-17 19:39:43 +0200

    feat: storage saving and restoring

--- CodeToSurvive/CodeToSurviveLib/Storage/StorageManagement.fs
+++ CodeToSurvive/CodeToSurviveLib/Storage/StorageManagement.fs
@@ -1,15 +1,20 @@
 namespace CodeToSurvive.Lib.Storage
 
+open System
 open System.IO
 open System.Runtime.Serialization.Json
 open System.Text
+open System.Text.RegularExpressions
 open CodeToSurvive.Lib.Core.Tick
 open CodeToSurvive.Lib.Storage.StoragePreference
 
 module StorageManagement =
 
-    let storageFileName x =
-        $"{x}.json"
+    let dateTimeFormat = "yyyy-MM-dd_HH:mm:ss"
+    let filePattern = "storage_(\d{4}-\d\d-\d\d_\d\d:\d\d:\d\d).json"
+    // storage-yyyy-MM-dd.json
+    let storageFileName x = $"storage_{x}.json"
+
     let serialize (state: 'a) : string =
         let serializer = DataContractJsonSerializer(typeof<'a>)
         let stream = new MemoryStream()
@@ -18,19 +23,24 @@ module StorageManagement =
         Encoding.UTF8.GetString(updateData)
 
     let deserialize (json: string) : 'a =
-        let instance = typedefof<'a>;
+        let instance = typedefof<'a>
         let ms = new MemoryStream(Encoding.Unicode.GetBytes(json))
 
-        let deserializer = DataContractJsonSerializer(instance);
-        let deserializedObj = deserializer.ReadObject(ms);
+        let deserializer = DataContractJsonSerializer(instance)
+        let deserializedObj = deserializer.ReadObject(ms)
         deserializedObj :?> 'a
-        
+
     let save (storage: IStoragePreference) (state: State) =
-        
-        ()
-        
+        let fileName = DateTime.Now.ToString dateTimeFormat |> storageFileName
+        let content = serialize state
+        let savePath = Path.Join(storage.StateStorageFolder.FullName, fileName)
+        File.WriteAllText(savePath, content)
+
     let loadNewest (storage: IStoragePreference) : State =
-        let stateStorage = storage.StateStorageFolder
-        // TODO get newestState
-        let loadedJson = ""
-        deserialize loadedJson
+        storage.StateStorageFolder.GetFiles()
+        |> Array.filter (fun dir -> Regex.IsMatch(dir.Name, filePattern))
+        |> Array.map (fun dir -> (dir, Regex.Match(dir.Name, filePattern).Value))
+        |> Array.maxBy snd
+        |> (fun (file, _) -> file.FullName)
+        |> File.ReadAllText
+        |> deserialize
